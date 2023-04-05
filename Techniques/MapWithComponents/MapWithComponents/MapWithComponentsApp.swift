6590ea06ea79849388e1727776cfb688a1c69f9e
NomoteteS <nomotetes.onetrue@icloud.com>
2023-04-06 02:58:56 +0300

    :computer: MapWithComponents is started

---
+++ Techniques/MapWithComponents/MapWithComponents/MapWithComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapWithComponentsApp.swift
+//  MapWithComponents
+//
+//  Created by NomoteteS on 7.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct MapWithComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
