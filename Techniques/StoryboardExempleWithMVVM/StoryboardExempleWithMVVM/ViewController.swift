ce92c7563d014774771815a55151dc051c948c5a
NomoteteS <nomotetes.onetrue@icloud.com>
2023-04-07 04:42:48 +0300

    :computer: StoryboardExempleWithMVVM is started

---
+++ Techniques/StoryboardExempleWithMVVM/StoryboardExempleWithMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryboardExempleWithMVVM
+//
+//  Created by NomoteteS on 7.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
